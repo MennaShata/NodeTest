57a4f0d620b42849e75991fadec53ce8039692b3
MennaShata <Mennashata10@gmail.com>
2018-04-21 16:18:46 +0200

    adding remote db

--- models/dbInitialize.js
+++ models/dbInitialize.js
@@ -1,4 +1,8 @@
 const mongoose = require ('mongoose');
+let mongo_url = 'mongodb://localhost/myapp';
 
- mongoose.connect('mongodb://localhost/myapp')
+if(proccess.env.NODE_ENV==='production'){
+    mongo_url="mongodb://<dbuser>:<dbpassword>@ds253879.mlab.com:53879/myapp"
+}
+ mongoose.connect(mongo_url)
 
