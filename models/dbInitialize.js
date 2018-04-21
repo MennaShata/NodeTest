2f0f9bac2f0b3b03f04661355a9ab17cc5046207
MennaShata <Mennashata10@gmail.com>
2018-04-21 16:25:18 +0200

    fix

--- models/dbInitialize.js
+++ models/dbInitialize.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
 let mongo_url = 'mongodb://localhost/myapp';
 
-if(proccess.env.NODE_ENV==='production'){
+if(process.env.NODE_ENV==='production'){
     mongo_url="mongodb://<dbuser>:<dbpassword>@ds253879.mlab.com:53879/myapp"
 }
  mongoose.connect(mongo_url)
